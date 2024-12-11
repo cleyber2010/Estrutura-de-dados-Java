e7e0ace70f86c20135fbdc3574a838c63aa2c04a
Cleyber <cleyber.fernandes@gmail.com>
2024-12-11 16:55:55 -0300

    Creation of the vector class, methods of adding and returning vector data. implementation of the Grow method for expanding array

--- src/br/com/cafeweb/estruturadados/main/Main.java
+++ src/br/com/cafeweb/estruturadados/main/Main.java
@@ -1,7 +1,36 @@
 package br.com.cafeweb.estruturadados.main;
 
+import br.com.cafeweb.estruturadados.vetor.Vetor;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("1. Vetores");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Selecione a opção: ");
+        var option = scanner.nextLine();
+
+        switch (option) {
+            case "1":
+                vetor();
+                break;
+        }
+    }
+
+    public static void vetor() {
+        Vetor<Integer> vetor = new Vetor<Integer>();
+
+        for (int i = 0; i < vetor.size(); i++) {
+            vetor.add(i + 1);
+        }
+
+        vetor.add(11);
+
+        System.out.println("============ Exibindo dados do vetor =================");
+
+        for (int i = 0; i < vetor.size(); i++) {
+            System.out.println(vetor.getVetor(i));
+        }
     }
 }
--- src/br/com/cafeweb/estruturadados/vetor/Vetor.java
+++ src/br/com/cafeweb/estruturadados/vetor/Vetor.java
@@ -1,4 +1,49 @@
 package br.com.cafeweb.estruturadados.vetor;
 
-public class Vetor {
+import java.util.Arrays;
+
+/**
+ * The vector class will have a generic type
+ * You will be responsible for manipulating the vectors
+ * Every class in Java inherits from the Object class
+ */
+
+public class Vetor<T> {
+    private Object[] vetor;
+    private int count;
+
+    /**
+     * Constructor
+     */
+    public Vetor() {
+        this.vetor = new Object[10];
+    }
+
+    public void add(T element ) {
+        if (count >= vetor.length) {
+            this.grow();
+        }
+
+        this.vetor[count] = element;
+        count++;
+    }
+
+    public void grow() {
+        this.vetor = Arrays.copyOf(this.vetor, this.size() + 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public T getVetor(int position) throws IndexOutOfBoundsException {
+        if (position < 0 || position >= this.size()) {
+            throw new ArrayIndexOutOfBoundsException(position);
+        }
+
+        return (T) this.vetor[position];
+    }
+
+    public int size() {
+        return vetor.length;
+    }
+
+
 }
